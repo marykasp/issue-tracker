99feadddb286c4291476d232e72c110d14db2468
marykasp <kasparian.mary@gmail.com>
2023-10-07 17:58:41 -0500

    style: add active link styles using classnames library

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,15 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
 
 const Navbar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
+
   const links = [
     {
       label: "Dashboard",
@@ -16,7 +23,7 @@ const Navbar = () => {
 
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">
+      <Link href="/" className="text-red-500">
         <AiFillBug />
       </Link>
       <ul className="flex space-x-6">
@@ -25,7 +32,11 @@ const Navbar = () => {
             <li key={link.href}>
               <Link
                 href={link.href}
-                className="text-zinc-500 hover:text-zinc-800 transition-hover"
+                className={classnames({
+                  "text-zinc-900": link.href === currentPath,
+                  "text-zinc-500": link.href !== currentPath,
+                  "hover:text-zinc-800 transition-hover": true,
+                })}
               >
                 {link.label}
               </Link>
