689ff29739ad6a3bb43632e85848b308001d001c
marykasp <kasparian.mary@gmail.com>
2023-10-07 17:29:10 -0500

    style: build the navigatio bar

---
+++ app/Navbar.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+
+const Navbar = () => {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "/",
+    },
+    {
+      label: "Issues",
+      href: "/issues",
+    },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => {
+          return (
+            <li key={link.href}>
+              <Link
+                href={link.href}
+                className="text-zinc-500 hover:text-zinc-800 transition-hover"
+              >
+                {link.label}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <div>
       <h1>Hello world</h1>
-    </main>
+    </div>
   );
 }
