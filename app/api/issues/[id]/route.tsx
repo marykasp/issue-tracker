32e90f83e99ebca5a106722bafd512d488dd3a53
marykasp <kasparian.mary@gmail.com>
2023-10-11 13:35:56 -0500

    feat: build an api for updating an existing issue

---
+++ app/api/issues/[id]/route.tsx
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import { issueSchema } from "../../../validationSchemas";
+import prisma from "@/prisma/client";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const body = await request.json();
+  // validate body of request
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  // if body of request is valid then find issue with given id
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  // if issue exists update issue
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-import { createIssueSchema } from "../../validationSchemas";
+import { issueSchema } from "../../validationSchemas";
 
 // export async function GET(request: NextRequest) {
 //   const issues = prisma.issue.findMany();
@@ -11,7 +11,7 @@ import { createIssueSchema } from "../../validationSchemas";
 export async function POST(request: NextRequest) {
   const body = await request.json();
   // validate body of request
-  const validation = createIssueSchema.safeParse(body);
+  const validation = issueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
 
