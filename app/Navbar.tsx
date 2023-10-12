b7c62da1dc57c315d14099c1b17189e8d94d6e2b
marykasp <kasparian.mary@gmail.com>
2023-10-12 12:03:40 -0500

    style: add log out and log in links in navbar
    Create authprovider and wrap body with component in order for useSession
    hook to work (shares status and information about user)

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,9 +5,13 @@ import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
 import classnames from "classnames";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 
 const Navbar = () => {
   const currentPath = usePathname();
+  // get current users info (name, email, image)
+  const { status, data: session } = useSession();
 
   const links = [
     {
@@ -43,6 +47,14 @@ const Navbar = () => {
           );
         })}
       </ul>
+      <Box>
+        {status === "authenticated" && (
+          <Link href="/api/auth/signout">Log out</Link>
+        )}
+        {status === "unauthenticated" && (
+          <Link href="/api/auth/signin">Log In</Link>
+        )}
+      </Box>
     </nav>
   );
 };
