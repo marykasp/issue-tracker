3a896ff2d58314f2b8940e5e7ec98e59d7e226a9
marykasp <kasparian.mary@gmail.com>
2023-10-15 10:08:40 -0500

    chore: change validation schema to make title, desc, and assigned user optional for editing an issue
    Update the api endpoint for patching an issue

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -1,6 +1,6 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { issueSchema } from "../../../validationSchemas";
+import { patchIssueSchema } from "../../../validationSchemas";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
 
@@ -8,16 +8,24 @@ export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } },
 ) {
-  const session = await getServerSession(authOptions);
-
-  // unauthorized status
-  if (!session) return NextResponse.json({}, { status: 401 });
+  // const session = await getServerSession(authOptions);
+  // // unauthorized status
+  // if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
   // validate body of request
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
+
+  // is user is in body of request check if valid user
+  if (body.assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: body.assignedToUserId },
+    });
+    if (!user)
+      return NextResponse.json({ error: "Invalid user" }, { status: 400 });
+  }
 
   // if body of request is valid then find issue with given id
   const issue = await prisma.issue.findUnique({
@@ -33,6 +41,7 @@ export async function PATCH(
     data: {
       title: body.title,
       description: body.description,
+      assignedToUserId: body.assignedToUserId,
     },
   });
 
