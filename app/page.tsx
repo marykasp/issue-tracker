72272014e28f496e8f60188893b08ec102e60c53
marykasp <kasparian.mary@gmail.com>
2023-10-16 12:57:03 -0500

    style: build the layout for pagination components
    Allow for user to navigate back to first or last page and increment
    between pages. Also displays the current page and total page count.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Pagination itemCount={100} pageSize={10} currentPage={10} />
     </div>
   );
 }
