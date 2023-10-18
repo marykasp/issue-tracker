cd161004dc8a7892712386d65d443cdeba86c3e6
marykasp <kasparian.mary@gmail.com>
2023-10-18 11:15:04 -0500

    style: layout dashboard with issue components

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import prisma from "../prisma/client";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 import IssueChart from "./IssueChart";
+import { Flex, Grid } from "@radix-ui/themes";
 
 const Home = async () => {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
@@ -10,7 +11,15 @@ const Home = async () => {
   });
   const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
 
-  return <IssueChart open={open} closed={closed} inProgress={inProgress} />;
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
 };
 
 export default Home;
